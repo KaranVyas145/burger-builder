0c71407c8dda98591a419b2cfe8d66c21619c68c
Karan Vyas <karanvyas2907@gmail.com>
2022-02-28 23:23:20 +0530

    created order summary

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,15 +1,17 @@
-import React from "react"
-import classes from "./BuildControl.css"
+import React from 'react';
 
-const buildControl=(props)=>(
+import classes from './BuildControl.css';
+
+const buildControl = (props) => (
     <div className={classes.BuildControl}>
-        <div className={classes.Label}>
-            {props.label}
-            <button className={classes.Less} onClick={props.removed} disabled={props.disabled}>Less</button>
-            <button className={classes.More} 
-            onClick={props.added}
-            >More</button>
-        </div>
+        <div className={classes.Label}>{props.label}</div>
+        <button 
+            className={classes.Less} 
+            onClick={props.removed} 
+            disabled={props.disabled}>Less</button>
+        <button 
+            className={classes.More} 
+            onClick={props.added}>More</button>
     </div>
 );
 
