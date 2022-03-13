fafd881068d58544e7ed00d64c667f5ad9fb8848
Karan Vyas <karanvyas2907@gmail.com>
2022-03-13 23:57:35 +0530

    fetched orders via redux

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -36,8 +36,50 @@ export const purchaseBurger = (orderData) => {
   };
 };
 
-export const purchaseInit = ()=>{
+export const purchaseInit = () => {
   return {
-    type: actionTypes.PURCHASE_INIT
-  }
-}
+    type: actionTypes.PURCHASE_INIT,
+  };
+};
+
+export const fetchOrdersSuccess = (orders) => {
+  return {
+    type: actionTypes.FETCH_ORDER_SUCCESS,
+    orders: orders,
+  };
+};
+
+export const fetchOrdersfail = (error) => {
+  return {
+    type: actionTypes.FETCH_ORDERS_FAIL,
+    error: error,
+  };
+};
+
+export const fetchOrdersStart = () => {
+  return {
+    type: actionTypes.FETCH_ORDER_START,
+  };
+};
+
+export const fetchOrders = () => {
+  return (dispatch) => {
+    dispatch(fetchOrdersStart())
+    axios
+      .get("/orders.json")
+      .then((res) => {
+        const fetchedOrders = [];
+        for (let key in res.data) {
+          fetchedOrders.push({
+            ...res.data[key],
+            id: key,
+          });
+        }
+        console.log(fetchedOrders);
+        dispatch(fetchOrdersSuccess(fetchedOrders));
+      })
+      .catch((err) => {
+        dispatch(fetchOrdersfail(err));
+      });
+  };
+};
--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -3,16 +3,16 @@ import * as actionTypes from "../actions/actionTypes";
 const initialState = {
   orders: [],
   loading: false,
-  purchased: false
+  purchased: false,
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.PURCHASE_INIT:
-      return{
+      return {
         ...state,
-        purchased:false
-      }
+        purchased: false,
+      };
     case actionTypes.PURCHASE_BURGER_START:
       return {
         ...state,
@@ -26,10 +26,26 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         loading: false,
-        purchased:true,
+        purchased: true,
         orders: state.orders.concat(newOrder),
       };
     case actionTypes.PURCHASE_BURGER_FAIL:
+      return {
+        ...state,
+        loading: false,
+      };
+    case actionTypes.FETCH_ORDER_START:
+      return {
+        ...state,
+        loading: true,
+      };
+    case actionTypes.FETCH_ORDER_SUCCESS:
+      return {
+        ...state,
+        orders: action.orders,
+        loading: false,
+      };
+    case actionTypes.FETCH_ORDERS_FAIL:
       return {
         ...state,
         loading: false,
